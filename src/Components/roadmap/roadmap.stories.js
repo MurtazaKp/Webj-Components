883002f33cf0e3102b4471fb61e3b3b84072def0
murtazakp <khopoliwala52@gmail.com>
2024-01-10 10:13:02 +0530

    feat(roadmap) : added second roadmap

--- src/Components/roadmap/roadmap.stories.js
+++ src/Components/roadmap/roadmap.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RoadmapOne from './roadmapOne';
+import RoadmapTwo from './roadmapTwo';
 
 
 export default {
@@ -42,38 +43,34 @@ roadmaps : [
 }
 
 
-const RoadmapTwoTemplate = (args) => <RoadmapTwoStory {...args} />;
+const RoadmapTwoTemplate = (args) => <RoadmapTwo {...args} />;
 
 export const RoadmapTwoStory = RoadmapTwoTemplate.bind({});
 
 RoadmapTwoStory.args = {
-    
-    heading: "The Roadmap",
-    description: "In a creative workplace, employees responsibly try different solutions",
-    roadmaps: [
-      {
-        imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Spark.svg",
-        title: "Phase 1",
-        subtitle: "Investment",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
-      },
-      {
-        imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Link.svg",
-        title: "Phase 2",
-        subtitle: "Connection",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
-      },
-      {
-        imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Setting.svg",
-        title: "Phase 3",
-        subtitle: "Setting up NFTS",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
-      },
-      {
-        imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Grow.svg",
-        title: "Phase 4",
-        subtitle: "Growth",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
-      }
-    ]
+   mainHeading: 'Our Roadmap',
+  mainDescription: 'In a creative workplace, employees with responsibly try different solutions',
+  mainImageSrc: 'https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/8/image.png',
+  phases: [
+    {
+      phase: 'Phase 1',
+      title: 'Launch',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
+    },
+    {
+      phase: 'Phase 2',
+      title: 'Investment',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
+    },
+    {
+      phase: 'Phase 3',
+      title: 'Connection',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
+    },
+    {
+      phase: 'Phase 4',
+      title: 'Growth',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
+    },
+  ],
 }
--- src/Components/roadmap/roadmaptwo.js
+++ src/Components/roadmap/roadmaptwo.js
@@ -1,69 +1,74 @@
 import React from 'react';
 
-// Declare the data
 const roadmapData = {
-  heading: "The Roadmap",
-  description: "In a creative workplace, employees responsibly try different solutions",
-  roadmaps: [
+  mainHeading: 'Our Roadmap',
+  mainDescription: 'In a creative workplace, employees with responsibly try different solutions',
+  mainImageSrc: 'https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/8/image.png',
+  phases: [
     {
-      imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Spark.svg",
-      title: "Phase 1",
-      subtitle: "Investment",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
+      phase: 'Phase 1',
+      title: 'Launch',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
     },
     {
-      imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Link.svg",
-      title: "Phase 2",
-      subtitle: "Connection",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
+      phase: 'Phase 2',
+      title: 'Investment',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
     },
     {
-      imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Setting.svg",
-      title: "Phase 3",
-      subtitle: "Setting up NFTS",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
+      phase: 'Phase 3',
+      title: 'Connection',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
     },
     {
-      imageSrc: "https://landingfoliocom.imgix.net/store/collection/niftyui/images/roadmap/3/Grow.svg",
-      title: "Phase 4",
-      subtitle: "Growth",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit odio sagittis, ac senectus eta."
-    }
-  ]
-}
+      phase: 'Phase 4',
+      title: 'Growth',
+      description: 'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Sit odio sagittis, ac senectus eta.',
+    },
+  ],
+};
 
-const RoadmapTwo = ({ heading, roadmaps, description }) => {
+const Roadmap = ({mainHeading,mainDescription,mainImageSrc,phases}) => {
   return (
-    <section className="py-12 bg-gray-900 sm:py-16 lg:py-20">
+    <section className="py-12 bg-white sm:py-16 lg:py-20">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-md text-center sm:text-left">
-          <h2 className="text-3xl font-bold text-white">
-            {heading}
-          </h2>
-          <p className="mt-4 text-base font-medium text-gray-400">
-            {description}
-          </p>
-        </div>
+        <div className="grid items-stretch grid-cols-1 gap-y-12 lg:grid-cols-12 gap-x-16">
+          <div className="lg:col-span-4">
+            <div className="aspect-w-4 aspect-h-3 lg:aspect-none">
+              <img className="object-cover w-full h-full" src={mainImageSrc} alt="" />
+            </div>
+          </div>
 
-        <div className="grid grid-cols-1 px-12 mt-12 text-center sm:mt-16 sm:px-0 sm:text-left sm:grid-cols-2 lg:grid-cols-4 gap-y-12 gap-x-16 xl:gap-x-20">
-          {roadmaps.map((item, index) => (
-            <div key={index}>
-              <img className="w-auto h-10 mx-auto sm:mx-0" src={item.imageSrc} alt="" />
-              <p className="mt-6 text-xl font-bold text-white">
-                {item.title}
-              </p>
-              <p className="mt-5 text-xs font-medium tracking-wide text-gray-500 uppercase">
-                {item.subtitle}
-              </p>
-              <p className="mt-3 text-sm font-medium text-gray-400">
-                {item.description}
+          <div className="flex flex-col justify-between lg:col-span-8">
+            <div className="max-w-md">
+              <h2 className="text-3xl font-bold text-gray-900">
+                {mainHeading}
+              </h2>
+              <p className="mt-4 text-base font-medium text-gray-500">
+                {mainDescription}
               </p>
             </div>
-          ))}
+
+            <div className="grid grid-cols-1 gap-12 mt-12 sm:grid-cols-2 sm:mt-16 xl:gap-x-16">
+              {phases.map((item, index) => (
+                <div key={index}>
+                  <p className="text-sm font-medium text-gray-500">
+                    {item.phase}
+                  </p>
+                  <p className="mt-4 text-xl font-bold text-gray-900">
+                    {item.title}
+                  </p>
+                  <p className="mt-4 text-base font-medium text-gray-500">
+                    {item.description}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
   );
 };
 
-export default RoadmapTwo;
+export default Roadmap;
