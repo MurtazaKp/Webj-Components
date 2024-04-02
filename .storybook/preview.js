df5e30f694142efae923a765f646ce4d3b00ed23
murtazakp <khopoliwala52@gmail.com>
2024-04-02 16:32:59 +0530

    feat fioxed

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 /** @type { import('@storybook/react').Preview } */
-import '../src/globals.css'
+import '../src/styles/globals.css'
 const preview = {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
