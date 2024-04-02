6069a29793a90ea974326260eb751dc387a228a0
murtazakp <khopoliwala52@gmail.com>
2024-04-02 15:24:24 +0530

    fixed the path

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,15 @@
 /** @type { import('@storybook/react').Preview } */
-import '../src/styles/globals.css';
+import '../src/styles/globals.css'
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
-};
+}
 
-export default preview;
+export default preview
